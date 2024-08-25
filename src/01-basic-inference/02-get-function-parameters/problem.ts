1631c45c8ab65f8f9479b2bca9a6d33d4a27620e
mladovic <marinladovic.develop@gmail.com>
2024-08-25 11:14:10 +0200

    unions, extract

--- src/01-basic-inference/02-get-function-parameters/problem.ts
+++ src/01-basic-inference/02-get-function-parameters/problem.ts
@@ -3,6 +3,8 @@ import { Equal, Expect } from '../../helpers'
 const castSpell = (caster: string, spell: string, manaCost: number) =>
   `${caster} casts ${spell} for ${manaCost} mana!`
 
+// ---- Don't touch above this line ----
+
 type CastSpellParams = unknown
 
 // ---- Don't touch below this line ----
--- src/01-basic-inference/02-get-function-parameters/solution.ts
+++ src/01-basic-inference/02-get-function-parameters/solution.ts
@@ -3,6 +3,8 @@ import { Equal, Expect } from '../../helpers'
 const castSpell = (caster: string, spell: string, manaCost: number) =>
   `${caster} casts ${spell} for ${manaCost} mana!`
 
+// ---- Don't touch above this line ----
+
 type CastSpellParams = Parameters<typeof castSpell>
 
 // ---- Don't touch below this line ----
